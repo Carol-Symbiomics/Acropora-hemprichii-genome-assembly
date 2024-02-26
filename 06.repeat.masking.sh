de05e814ee3e26b830c2e0c0e75f2194c447d6ab
Abdoallah Sharaf <abdoallah.sharaf@gmail.com>
2024-02-26 17:50:46 +0100

    Update 06.repeat.masking.sh

--- 06.repeat.masking.sh
+++ 06.repeat.masking.sh
@@ -4,15 +4,14 @@
 ## Identifying and masking Repeats
 # Repeats in Ahemp and available Acropora's coral genomes using RepeatModeler
 
-singularity pull dfam-tetools-latest.sif docker://dfam/tetools:latest
-singularity run dfam-tetools-latest.sif BuildDatabase -name Ahemp_genome Ahemp.gapclosed_f2.fasta
-singularity run dfam-tetools-latest.sif RepeatModeler -database Ahemp_genome -LTRStruct -threads 40
+docker run -v $PWD:/in -w /in dfam/tetools:latest BuildDatabase -name Ahemp_genome Ahemp.gapclosed_f2.fasta
+docker run -v $PWD:/in -w /in dfam/tetools:latest RepeatModeler -database Ahemp_genome -LTRStruct -threads 40
 
 # available Repeats in Acropora's coral genomes
 for i in `ls *.fna|sed 's/_genomic.fna//g`;
 do
-    singularity run ../../dfam-tetools-latest.sif BuildDatabase -name $i ${i}_genomic.fna
-    singularity run ../../dfam-tetools-latest.sif RepeatModeler -database $i -LTRStruct -threads 40;
+    docker run -v $PWD:/in -w /in dfam/tetools:latest BuildDatabase -name $i ${i}_genomic.fna
+    docker run -v $PWD:/in -w /in dfam/tetools:latest RepeatModeler -database $i -LTRStruct -threads 40;
 done
 
 # Repeats database assembly
@@ -20,7 +19,10 @@ cat *-families.fa > Acropora_RE_DB.fsa
 unsearch -fastx_uniques Acropora_RE_DB.fsa -fastaout Acropora_RE_DB.faa
 
 # Repeats masking in Ahemp using Acropora repeats DB
-singularity run dfam-tetools-latest.sif RepeatMasker Ahemp.gapclosed_f2.fasta -lib Acropora_RE_DB.faa -pa 8 -norna -nolow -xsmall
+docker run -v $PWD:/in -w /in dfam/tetools:latest RepeatMasker Ahemp.gapclosed_f2.fasta -lib Acropora_RE_DB.faa -pa 8 -norna -nolow -xsmall
 
 ## How is the distribution of repeats by types
 grep '>' Ahemp_genome_f2-families.fa | sed -r 's/.+#//' | sed -r 's/\s+.+//' | sort | uniq -c
+
+- The full report of the repeats percentage will outputted after masking in Ahemp.gapclosed_f2.fasta.tbl file
+
