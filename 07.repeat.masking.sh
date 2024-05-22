410c41b19f4b249f94f6b156bf834139a1def285
Anna Fiesinger <123569008+afiesinger@users.noreply.github.com>
2024-05-22 11:19:36 +0200

    Update 07.repeat.masking.sh

--- 07.repeat.masking.sh
+++ 07.repeat.masking.sh
@@ -1,28 +1,34 @@
-## Identify rRNA
-./barrnap -q -k euk Ahemp.gapclosed_f2.fasta --threads 50 --outseq Ahemp_rrna.fasta > Ahemp_rrna.gff 
+# # Identify rRNA # #
+barrnap -q -k euk Ahemp.RedSea.gaplosed_f2.ukon.fasta --threads 50 --outseq Ahemp_rrna.fasta > Ahemp_rrna.gff 
 
-## Identifying and masking Repeats
-# Repeats in Ahemp and available Acropora's coral genomes using RepeatModeler
+# # Docker Images # #
+docker pull dfam/tetools:latest
+docker pull oushujun/edta:2.0.0
 
-docker run -v $PWD:/in -w /in dfam/tetools:latest BuildDatabase -name Ahemp_genome Ahemp.gapclosed_f2.fasta
+# # Identifying Repeats using RepeatModeler # #
+docker run -v $PWD:/in -w /in dfam/tetools:latest BuildDatabase -name Ahemp_genome Ahemp.RedSea.gaplosed_f2.ukon.fasta
 docker run -v $PWD:/in -w /in dfam/tetools:latest RepeatModeler -database Ahemp_genome -LTRStruct -threads 40
 
-# available Repeats in Acropora's coral genomes
-for i in `ls *.fna|sed 's/_genomic.fna//g`;
+for i in `ls *.fna|sed 's/_genomic.fna//g';
 do
     docker run -v $PWD:/in -w /in dfam/tetools:latest BuildDatabase -name $i ${i}_genomic.fna
     docker run -v $PWD:/in -w /in dfam/tetools:latest RepeatModeler -database $i -LTRStruct -threads 40;
 done
 
-# Repeats database assembly
-cat *-families.fa > Acropora_RE_DB.fsa
-unsearch -fastx_uniques Acropora_RE_DB.fsa -fastaout Acropora_RE_DB.faa
+# # Identifying Repeats using EDTA # #
+docker run -v $PWD:/in -w /in biocontainers/edta:2.0.0 EDTA.pl --genome Ahemp.RedSea.gaplosed_f2.ukon.fasta --sensitive 1 --anno 1 -t 32
 
-# Repeats masking in Ahemp using Acropora repeats DB
-docker run -v $PWD:/in -w /in dfam/tetools:latest RepeatMasker Ahemp.gapclosed_f2.fasta -lib Acropora_RE_DB.faa -pa 8 -norna -nolow -xsmall
+# # Combine Repeats from EDTA and RepeatModeler # #
+cat *-families.fa *.mod.EDTA.TElib.fa > Ahemp_RE_DB.fsa
 
-## How is the distribution of repeats by types
-grep '>' Ahemp_genome_f2-families.fa | sed -r 's/.+#//' | sed -r 's/\s+.+//' | sort | uniq -c
+# #  Remove duplicates from repeats db # #
+unsearch -fastx_uniques Ahemp_RE_DB.fsa -fastaout Ahemp_RE_DB.fasta
 
-- The full report of the repeats percentage will outputted after masking in Ahemp.gapclosed_f2.fasta.tbl file
+# # Repeats masking using Repeats DB from previous step # #
+docker run -v $PWD:/in -w /in dfam/tetools:latest RepeatMasker Ahemp.RedSea.gaplosed_f2.ukon.fasta -lib Ahemp_RE_DB.faa -pa 8 -norna -nolow -xsmall
+
+# # Get Repeat distribution # #
+grep '>' Ahemp_repeat_families.fa | sed -r 's/.+#//' | sed -r 's/\s+.+//' | sort | uniq -c
+
+# The full report of the repeats percentage will outputted after masking in xx.RepeatMasker.fasta.tbl file
 
