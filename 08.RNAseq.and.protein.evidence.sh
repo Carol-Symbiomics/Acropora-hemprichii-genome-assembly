c53d2c98af03873c31c5ce5567973c7dd21ca217
Anna Fiesinger <123569008+afiesinger@users.noreply.github.com>
2024-05-22 11:20:21 +0200

    Update 08.RNAseq.and.protein.evidence.sh

--- 08.RNAseq.and.protein.evidence.sh
+++ 08.RNAseq.and.protein.evidence.sh
@@ -15,7 +15,7 @@ conda install -c bioconda springtie
 conda install transdecoder
 
 ## Indexing the assembled genome
-STAR --runThreadN 50 --runMode genomeGenerate --genomeDir Ahemp_index --genomeFastaFiles Ahemp.gapclosed_f2.fasta --genomeSAindexNbases 10
+STAR --runThreadN 50 --runMode genomeGenerate --genomeDir Ahemp_index --genomeFastaFiles Ahemp.RedSea.gaplosed_f2.ukon.fasta --genomeSAindexNbases 10
 
 ## Mapping the RNA-Seq reads to the assembled genome
 STAR --runThreadN 30 --genomeDir Ahemp_index --readFilesIn M_19_2595_HE1-33-T1_D703-D505_L008_R1_001.fastq.gz M_19_2595_HE1-33-T1_D703-D505_L008_R2_001.fastq.gz --readFilesCommand "gunzip -c" --outSAMtype  BAM SortedByCoordinate --outSAMstrandField intronMotif --outFilterIntronMotifs RemoveNoncanonical --outFileNamePrefix M_19_2595_HE1-33-T1_D703-D505_ --limitBAMsortRAM 10000000000
@@ -34,7 +34,7 @@ stringtie -p 30 -o Ahemp_RNASeqAll.Stringtie.gtf Ahemp_RNASeqAll.STAR.bam
 grep -v "#" Ahemp_RNASeqAll.Stringtie.gtf  | cut -f3 | sort | uniq -c
 
 ## Preparing transcripts evidence
-gtf_genome_to_cdna_fasta.pl Ahemp_RNASeqAll.Stringtie.gtf Ahemp.gapclosed_f2.fasta > Ahemp_RNASeqAll.transcripts.fasta
+gtf_genome_to_cdna_fasta.pl Ahemp_RNASeqAll.Stringtie.gtf Ahemp.RedSea.gaplosed_f2.ukon.fasta > Ahemp_RNASeqAll.transcripts.fasta
 
 ## Preparing protein evidence
 # Search for curated proteins sequences for "Acropora" on UniProt database and save them as "uniprot_Acropora.faa"
